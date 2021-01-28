0193666b01a724edf7fb49bcdd077abda30ba533
shashankmittra <44288289+shashankmittra@users.noreply.github.com>
2021-01-28 18:10:11 +0530

    ss

--- vars/first.groovy
+++ vars/first.groovy
@@ -1,3 +1,16 @@
-def clone(){
-	 ["git", "clone", "https://github.com/shashankmittra/simple-text.git"].execute()
+String branchName = env.BRANCH_NAME
+String repoUrl = "https://github.com/shashankmittra/simple-text.git"
+
+node {
+  // Start Stages
+  stage('Clone') {
+      // Clones the repository from the current branch name
+      echo 'Make the output directory'
+      sh 'mkdir -p build'
+
+      echo 'Cloning files from (branch: "' + branchName + '" )'
+      dir('build') {
+          git branch: branchName, url: repoUrl
+      }     
+  }       
 }
