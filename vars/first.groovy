4ddf773549ebc011251afd183c02902baa85b403
shashankmittra <44288289+shashankmittra@users.noreply.github.com>
2021-01-28 18:12:40 +0530

    tg

--- vars/first.groovy
+++ vars/first.groovy
@@ -1,4 +1,5 @@
-import java.lang.String java.lang.Object 
+import java.lang.String 
+import java.lang.Object 
 
 def clone(){
 	 ["git", "clone", "https://github.com/shashankmittra/simple-text.git"].execute()
