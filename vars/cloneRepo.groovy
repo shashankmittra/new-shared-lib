1d24d522fc07e941f88519cd7a9fb67cd26e47eb
shashankmittra <44288289+shashankmittra@users.noreply.github.com>
2021-02-01 09:44:54 +0530

    dsd

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,4 +1,4 @@
-def call(string branchName, string gitUrl){
+def call(def branchName, def gitUrl){
 	dir('cloned'){
 		checkout([$class: 'GitSCM', branches: [[name: branchName]], userRemoteConfigs: [[url: gitUrl]]])
 	}
